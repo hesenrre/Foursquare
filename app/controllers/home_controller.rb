da59713492896517403b41bb483ef5ec04ece407
Alfonso Balbuena Sánchez <alfonso1011@gmail.com>
2013-06-10 22:04:26 -0500

    Mapa reducido
    Se pone el mapa en un div y se agregan controles de html

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,27 @@ class HomeController < ApplicationController
  def load_venues
     lat = params[:lat]
     lng = params[:lng]
+    search = params[:search]
+    
   	puts 'Load venues'
   	client = Foursquare2::Client.new(:client_id => 'YCGWKAG00MWPLIEBBEBG3OQOWAOWHAEJHCQXLVG2DJIAQ4WR', :client_secret => 'TQBALNIDAMHZVBWJKUCA34KSWYVTXFMLS4AJOHQLQGDXPBAD')
     coords = lat + "," + lng
-    venues = client.search_venues(:ll => coords)
+    radius = 500
+
+    if search.is_a?(NilClass)
+      venues = client.search_venues(:ll => coords, :intent => 'browse', :radius => radius, :limit => 50)
+    else
+      venues = client.search_venues(:ll => coords, :intent => 'browse', :radius => radius, :limit => 50, :query => search)
+    end
+
+
+    
+    #prueba = client.search_venues_by_tip(:ll => coords, :radius => radius)
+    #puts prueba
     items = venues['groups'][0]['items']
+    #items.each{|venue| puts venue
+     #         puts ""}
+
     respond_to do |format|
       format.json {render json: items}
     end
